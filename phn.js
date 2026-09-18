19642efcf447e01d6888e623bb08568abc40a816
yetzt <git@yetzt.me>
2026-09-18 15:09:28 +0200

    eliminate this

--- phn.js
+++ phn.js
@@ -98,7 +98,7 @@ process.on("exit", ()=>{
 });
 
 // phn
-const phn = async (opts, fn)=>{
+const phn = async function(opts, fn){
 
 	// callback compat
 	if (typeof fn === "function") return await phn(opts).then(data=>(fn(null, data))).catch(fn);
@@ -106,82 +106,82 @@ const phn = async (opts, fn)=>{
 	if (typeof opts === "string") opts = { url: opts };
 	if (!("url" in opts) || !opts.url) throw new Error("Missing url option from options for request method.");
 
-	this.url = (typeof opts.url === "string") ? new URL(opts.url) : opts.url;
-	this.method = (opts.method || "get").toUpperCase();
-	this.data = null;
+	const url = (typeof opts.url === "string") ? new URL(opts.url) : opts.url;
+	const method = (opts.method || "get").toUpperCase();
+	let data = null;
 
 	// maximum buffer size
-	this.maxBuffer = parseInt(opts.maxBuffer,10) || Infinity;
+	const maxBuffer = parseInt(opts.maxBuffer,10) || Infinity;
 
 	// max redirects
-	this.maxRedirects = (typeof opts?.maxRedirects === "number") ? opts.maxRedirects : (typeof opts?.follow === "number") ? opts.follow : 20;
+	const maxRedirects = (typeof opts?.maxRedirects === "number") ? opts.maxRedirects : (typeof opts?.follow === "number") ? opts.follow : 20;
 	opts.redirected = opts.redirected || 0;
 
 	// http2 options
-	this.http2core = (typeof opts.http2 === "object") ? opts.http2 : {};
+	const http2core = (typeof opts.http2 === "object") ? opts.http2 : {};
 
 	// headers
-	this.headers = {};
-	if (opts.headers) for (const [k,v] of Object.entries(opts.headers)) this.headers[k.toLowerCase()] = v;
+	const headers = {};
+	if (opts.headers) for (const [k,v] of Object.entries(opts.headers)) headers[k.toLowerCase()] = v;
 
 	// query
-	if (opts.query) for (const [k,v] of Object.entries(opts.headers)) this.url.searchParams.append(k,v);
+	if (opts.query) for (const [k,v] of Object.entries(opts.headers)) url.searchParams.append(k,v);
 
 	// form
 	if (opts.form) {
-		this.data = qs.stringify(opts.form);
-		this.headers["content-type"] = "application/x-www-form-urlencoded";
+		data = qs.stringify(opts.form);
+		headers["content-type"] = "application/x-www-form-urlencoded";
 	};
 
 	// data
 	if (opts.data) {
 		if (typeof opts.data === "object" && !Buffer.isBuffer(opts.data) && !ArrayBuffer.isView(opts.data)) { // json
-			this.data = JSON.stringify(opts.data);
-			this.headers["content-type"] = "application/json";
+			data = JSON.stringify(opts.data);
+			headers["content-type"] = "application/json";
 		} else {
-			this.data = opts.data;
-			if (!this.headers["content-type"]) this.headers["content-type"] = "application/octet-stream";
+			data = opts.data;
+			if (!headers["content-type"]) headers["content-type"] = "application/octet-stream";
 		}
 	};
 
 	// set content-length
-	if (this.data && !this.headers["content-length"]) this.headers["content-length"] = Buffer.byteLength(this.data);
+	if (data && !headers["content-length"]) headers["content-length"] = Buffer.byteLength(data);
 
 	// compression, set unless explicitly off
-	if ((!("compression" in opts) || !!opts.compression) && !this.headers["accept-encoding"]) this.headers["accept-encoding"] = (typeof opts.compression === "string") ? opts.compression : supportedCompression;
+	if ((!("compression" in opts) || !!opts.compression) && !headers["accept-encoding"]) headers["accept-encoding"] = (typeof opts.compression === "string") ? opts.compression : supportedCompression;
 
 	// send request
 	let { transport, req, res, stream, client } = await new Promise(async (resolve, reject)=>{
 
 		// assemble options for http1
 		const options = {
-			protocol: this.url.protocol,
-			host: this.url.hostname.replace("[", "").replace("]", ""),
-			port: this.url.port,
-			path: this.url.pathname + (this.url.search ?? ""),
-			method: this.method,
-			headers: this.headers,
-			agent: httpAgent(this.url.protocol),
+			protocol: url.protocol,
+			host: url.hostname.replace("[", "").replace("]", ""),
+			port: url.port,
+			path: url.pathname + (url.search ?? ""),
+			method,
+			headers,
+			agent: httpAgent(url.protocol),
 			...opts.core,
 		};
 
 		let req;
-		switch (this.url.protocol) {
+		switch (url.protocol) {
 			case "http:":
 				req = http.request(options, res=>resolve({ transport: "http", req, res, stream: res }));
 			break;
 			case "https:":
 
 				// use http2 if module is loaded, http2 not explicitly off and available on host
-				if (http2 && (!("http2" in opts) || !!opts.http2) && ("h2" === await alpn(this.url))) {
+				if (http2 && (!("http2" in opts) || !!opts.http2) && ("h2" === await alpn(url))) {
 
 					// new http2 session
-					const client = await http2Session(this.url, this.coreOptions);
+					const client = await http2Session(url);
 
 					// reference socket
 					client.socket.ref();
 
-					req = client.request({ ":method": options.method, ":path": options.path, ...options.headers, ...this.http2core });
+					req = client.request({ ":method": options.method, ":path": options.path, ...options.headers, ...http2core });
 
 					req.on("response", (headers) => {
 						const res = { headers, statusCode: headers[":status"] };
@@ -197,7 +197,7 @@ const phn = async (opts, fn)=>{
 
 			break;
 			default:
-				return reject(new Error(`Bad protocol: ${this.url.protocol}`));
+				return reject(new Error(`Bad protocol: ${url.protocol}`));
 			break;
 		};
 
@@ -212,7 +212,7 @@ const phn = async (opts, fn)=>{
 		req.on("error", reject);
 
 		// send data
-		if (this.data) req.write(this.data);
+		if (data) req.write(data);
 
 		// end request
 		req.end();
@@ -226,10 +226,10 @@ const phn = async (opts, fn)=>{
 	if (res.headers?.location && (opts.follow || opts.followRedirects)) {
 
 		// limit the number of redirects
-		if (this.maxRedirects && ++opts.redirected > this.maxRedirects) throw new Error("Exceeded the maximum number of redirects");
+		if (maxRedirects && ++opts.redirected > maxRedirects) throw new Error("Exceeded the maximum number of redirects");
 
-		const redirectedUrl = new URL(res.headers["location"], this.url);
-		if (redirectedUrl.protocol === this.url.protocol && redirectedUrl.host === this.url.host) { // keep cookies
+		const redirectedUrl = new URL(res.headers["location"], url);
+		if (redirectedUrl.protocol === url.protocol && redirectedUrl.host === url.host) { // keep cookies
 			if (res.headers["set-cookie"]) opts.headers = { ...opts.headers, cookie: res.headers["set-cookie"] };
 		} else { // remove spicy request headers
 			opts.headers = Object.entries({ ...opts.headers }).reduce((h,[k,v])=>{
@@ -244,7 +244,7 @@ const phn = async (opts, fn)=>{
 	};
 
 	// check content-length header against maxBuffer
-	if (res.headers["content-length"] && parseInt(res.headers["content-length"],10) > this.maxBuffer) {
+	if (res.headers["content-length"] && parseInt(res.headers["content-length"],10) > maxBuffer) {
 		throw new Error(`Content length exceeds maxBuffer: ${res.headers["content-length"]}b`);
 	};
 
